b9143bfcbb169107bc1d27892391c602646e5188
heeeione <heewon410@naver.com>
2025-06-26 18:50:55 +0900

    feat: Add core

---
+++ src/core/iBlockchain.ts
@@ -0,0 +1,42 @@
+import { Option } from '../libs/jinx/option'
+import { Result } from '../libs/jinx/result'
+import { Block } from '../types/Block'
+import { Transaction } from '../types/Transaction'
+import { CoreError } from './error'
+
+export interface IBlockchain {
+    get currentNodeUrl(): string
+    networkNodes: string[]
+    get lastBlock(): Block
+    pendingTransactions: Transaction[]
+    chain: Block[]
+    createNewBlock(nonce: number, previousBlockHash: string, hash: string): Block
+    createNewTransaction(
+        amount: number,
+        sender: string,
+        recipient: string
+    ): Result<Transaction, CoreError>
+    addTransactionToPendingTransactions(
+        transaction: Transaction
+    ): Result<number, CoreError>
+    hashBlock(
+        previousBlockHash: string,
+        currentBlockData: BlockData,
+        nonce: number
+    ): string
+    proofOfWork(
+        previousBlockHash: string,
+        currentBlockData: BlockData
+    ): Result<number, CoreError>
+    chainIsValid(blockchain: Block[]): Result<{}, CoreError>
+    getBlock(blockHash: string): Option<Block>
+    getTransaction(
+        transactionId: string
+    ): Option<{ block: Block; transaction: Transaction }>
+    getAddressData(address: string): Option<{
+        balance: number
+        transactions: Transaction[]
+    }>
+}
+
+export type BlockData = { index: number; transactions: Transaction[] }
